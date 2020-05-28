9594a674483a2fd4e5383fda8c35c539c5ade054
Christian Tietze <christian.tietze@gmail.com>
2020-05-28 15:39:40 +0200

    beautify the output a little bit

--- index.php
+++ index.php
@@ -31,24 +31,47 @@ foreach ($urls as $url) {
     $all_pages[$url] = obtain_now_page($url);
 }
 
-// Render Table of Contents
+// Render minimal CSS
+?>
+<style type="text/css">
+body {
+    font-size: 18px;
+    background: #ddd;
+}
+body > article,
+body > section {
+    max-width: 50em;
+    margin: 3em auto;
+}
+.now_page_item {
+    background: #fff;
+    padding: 1em 1em;
+    border-radius: 0.2em;
+}
+</style>
+<?php
+
+// Render Table of Contents.
+echo '<section class="item toc">';
 echo "<h1>Table of Contents</h1>";
 echo "<ul>";
 $i = 0;
 foreach ($all_pages as $url => $content) {
     $i++;?>
-    <li><a href="#<?=$i?>"><?=$url?></a></li>
+    <li><a href="#_<?=$i?>"><?=$url?></a></li>
 <?php }
 echo "</ul>";
+echo "</section>";
 
 
-// Render actual pages
+// Render actual pages.
 $i = 0;
 foreach ($all_pages as $url => $content) {
     $i++;?>
-    <hr>
-    <article>
-    <h1 name="<?=$i?>">From: <?=$url?></h1>
-    <?php echo $content; ?>
-    </article>
+    <section>
+      <h1 id="_<?=$i?>">From: <?=$url?></h1>
+      <article class="now_page_item">
+        <?php echo $content; ?>
+      </article>
+    </section>
 <?php }
